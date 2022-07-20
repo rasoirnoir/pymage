b1711b28a32c003897b0aae2e6e86124a7de32b7
William Noris <w.noris@protonmail.com>
2022-07-20 21:49:42 +0200

    Basic convert en dur

--- pymage.py
+++ pymage.py
@@ -1,9 +1,24 @@
-from ast import main
+"""Image manipulation in python
 
+Image manipulation like resizing for a large number of
+images at the same time.
 
-if __name__ == "__main__":
-    main()
+Author: William Noris w.noris@protonmail.com
+Created: 20/07/2022
+"""
+import math
+from PIL import Image
 
 
 def main():
-    print("Bienvenue dans e programme de manipulation d'images.")
+    print("Bienvenue dans le programme de manipulation d'images.")
+    img = Image.open("img.jpg")
+    (w, h) = img.size
+    newSize = (math.floor(w/2), math.floor(h/2))
+    imgResized = img.resize(newSize)
+    imgResized.save("img-copie.jpg")
+
+
+if __name__ == "__main__":
+    print("coucou")
+    main()
