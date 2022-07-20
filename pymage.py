61be57227e05ade5c55b3101d188c14bd3801243
William Noris <w.noris@protonmail.com>
2022-07-20 23:21:20 +0200

    argparse ajouté

--- pymage.py
+++ pymage.py
@@ -6,8 +6,33 @@ images at the same time.
 Author: William Noris w.noris@protonmail.com
 Created: 20/07/2022
 """
+from ast import parse
 import math
+import argparse
 from PIL import Image
+
+_FILES = "./"
+
+
+def resize():
+    print(f"Resize the image !! {_FILES}")
+
+
+def parse():
+    # Top level parser
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(
+        title='subcommands', description='valid subcommands', help='additional help')
+
+    # parsers for subcommands
+    parserResize = subparsers.add_parser('resize')
+    parserResize.add_argument(
+        '-f', '--files', help="Dossier, fichier, ou liste de fichies séparés par des virgules")
+    parserResize.set_defaults(func=resize)
+    args = parser.parse_args()
+    _FILES = args.files
+    print(f"_FILES: {_FILES}, argument: {args.files}")
+    args.func()
 
 
 def main():
@@ -20,5 +45,5 @@ def main():
 
 
 if __name__ == "__main__":
-    print("coucou")
+    parse()
     main()
